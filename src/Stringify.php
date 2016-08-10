230f9b32cb54190b22c98e3d8f64e1cf5749b737
Jasper Jules Balbuena <jasperjules.balbuena@bicol-u.edu.ph>
2016-08-10 20:40:05 +0800

    Update Stringify.php

--- src/Stringify.php
+++ src/Stringify.php
@@ -32,7 +32,6 @@ class Stringify
 	/**
 	* Convert a CamelCase string to snake_case
 	*
-	* @param string
 	* @return object
 	*/
 	public function camelToSnake()
@@ -49,21 +48,6 @@ class Stringify
 	}
 
 	/**
-	* Uppercase every first letter of every word
-	*
-	* @param string
-	* @return object
-	*/
-	public function capitalize()
-	{
-		$string = "";
-		foreach(explode(" ", $this->string) as $value) {
-			$string .= ' ' . ucfirst($value);
-		}
-		return $string;
-	}
-
-	/**
 	* Name constructor
 	*
 	* @param string
@@ -73,132 +57,76 @@ class Stringify
 	{
 		return new static($string);
 	}
+
+	/**
+	* Convert sentence to slug
+	*
+	* @return object
+	*/
+	public function toSlug()
+	{
+		return strtolower(str_replace(' ', '-', trim($this->string)));
+	}
+
+	/**
+	* Transform string to lower/upper case
+	*
+	* @param int $type
+	* @param string $position
+	* @return object
+	*/
+	public function transform($type = 1, $position = 'all')
+	{
+		if( $type == 0 ) { // Lowercase
+			try {
+				if($position == 'all') {
+					return strtolower($this->string);
+				}
+				else if($position >= 1) {
+					$position--; // So that counter starts at 1
+					foreach(str_split($this->string) as $key => $value) {
+						if($position == $key) $this->string[$position] = strtolower($value);
+					}
+
+					return $this->string;
+				}
+			} catch (Exception $e) {
+
+			}
+		}
+		else { // Uppercase
+			try {
+				if($position == 'all') {
+					return strtoupper($this->string);
+				}
+				else if($position >= 1) {
+					foreach(str_split($this->string) as $key => $value) {
+						if($position == $key) $this->string[$position] = strtoupper($value);
+					}
+
+					return $this->string;
+				}
+			} catch (Exception $e) {
+				
+			}
+		}
+	}
+
+	/**
+	* Transform words to lower/upper case
+	*
+	* @param int $type
+	* @param string $position
+	* @return object
+	**/
+	public function transformWords($type = 1, $position = 'all') {
+		$string = "";
+
+		foreach(explode(" ", $this->string) as $value) {
+			$string .= ' ' . Stringify::create($value)->transform($type, $position);
+		}
+
+		return $string;
+	}
 }
 
-echo Stringify::create('hello world!')->capitalize();
-
-/*
-
-htmlEntities($flags = ENT_HTML5, $encoding = 'UTF-8', $doubleEncode = true)
-
-Manipulator::make('&')->htmlEntities();
-// &amp;
-htmlEntitiesDecode($flags = ENT_HTML5, $encoding = 'UTF-8')
-
-Manipulator::make('&amp;')->htmlEntitiesDecode();
-// &
-htmlSpecialCharacters($flags = ENT_HTML5, $encoding = 'UTF-8', $doubleEncode = true)
-
-Manipulator::make('&<>')->htmlSpecialCharacters();
-// &amp;&lt;&gt;
-lowercaseFirst
-
-Manipulator::make('HELLO')->lowercaseFirst();
-// hELLO
-make($string)
-
-// Named constructor
-Manipulator::make('string');
-pad($length, $string, $type = null)
-
-Manipulator::make('Hello')->pad(2, '!!', STR_PAD_RIGHT);
-// Hello!!
-prepend($string)
-
-Manipulator::make('is the one.')->prepend('Neo ');
-// Neo is the one.
-pluralize($items = null)
-
-Manipulator::make('Potato')->pluralize();
-// Potatoes
-You can optionally pass an array or numeric value to pluaralize to determine if the given string should be pluaralized.
-
-$dogs = ['Zoe', 'Spot', 'Pickles'];
-Manipulator::make('Dog')->pluralize($dogs);
-// Dogs
-
-$cats = ['Whiskers'];
-Manipulator::make('Cat')->pluralize($cats);
-// Cat
-remove($string, $caseSensitive = true)
-
-Manipulator::make('Dog Gone')->remove('Gone');
-// Dog
-removeSpecialCharacters($exceptions = [])
-
-Manipulator::make('Hello!!')->removeSpecialCharacters();
-// Hello
-Manipulator::make('Hello!!')->removeSpecialCharacters(['!']);
-// Hello!!
-repeat($multiplier = 1)
-
-Manipulator::make('la')->repeat(3);
-// lalala
-replace($find, $replace = '', $caseSensitive = true)
-
-Manipulator::make('Pickles are good.')->replace('good', 'terrible');
-// Pickles are terrible.
-reverse
-
-Manipulator::make('Whoa!')->reverse();
-// !aohW
-snakeToCamel
-
-Manipulator::make('snake_case')->snakeToCamel();
-// snakeCase
-snakeToClass
-
-stripTags($allowed = '')
-
-Manipulator::make('<i>Hello</i>')->stripTags();
-// Hello
-toCamelCase
-
-Manipulator::make('camel case')->toCamelCase();
-// camelCase
-toLower
-
-Manipulator::make('LOWER')->toLower();
-// lower
-toSlug
-
-Manipulator::make('This is a slug!')->toSlug();
-// this-is-a-slug
-toSnakeCase
-
-Manipulator::make('snake case')->toSnakeCase();
-// snake_case
-toString
-
-This method just returns the string.
-
-toUpper
-
-Manipulator::make('upper')->toUpper();
-// UPPER
-trim
-
-Manipulator::make('  trimmed  ')->trim();
-// trimmed
-trimBeginning
-
-Manipulator::make('  trimmed')->trimBeginning();
-// trimmed
-trimEnd
-
-Manipulator::make('trimmed  ')->trimEnd();
-// trimmed
-truncate($length = 100, $append = '...')
-
-Manipulator:make('This is a sentence and will be truncated.')->truncate(10, '...');
-// This is a ...
-urlDecode
-
-Manipulator::make('hello%21')->urlDecode();
-// hello!
-urlEncode
-
-Manipulator::make('hello!')->urlEncode();
-// hello%21
-
-*/
